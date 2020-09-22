b3b400fdf4c0c1c5eea243cbafb9d5064ff37eb9
Ajay <ajodedra16@gmail.com>
2020-09-22 22:22:00 +0100

    Initial Commit

---
+++ MarvelHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHeroApp
+//
+//  Created by Ajay Odedra on 22/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
